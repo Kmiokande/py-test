d8463fcc796b8a6ab416f47737554534121a46a1
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-02-09 16:48:03 -0300

    Adicionando test_tdd_example

---
+++ test_tdd_example.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+def fun(a, b):
+    return a + b
+
+
+class MyTest(unittest.TestCase):
+    def test_sum_positive_numbers(self):
+        self.assertEqual(fun(1, 1), 2)
+
+    def test_letter_sum(self):
+        with self.assertRaises(ValueError) as error:
+            fun('a', 'b')
+            self.assertSetEqual(error.msg, "Only numbers are allowed!")
+
+
+if __name__ == '__main__':
+    unittest.main()
