e19ccd353e323cc0c5d1c69700e20da02174a762
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-02-09 12:24:54 -0300

    Adicionando editoconfig; Adicionando o primeiro exemplo de teste;

---
+++ test_example.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+def fun(x):
+    return x + 1
+
+
+class MyTest(unittest.TestCase):
+    def test_that_fun_adds_onde(self):
+        self.assertEqual(fun(3), 4)
+
+
+class MySecondTest(unittest.TestCase):
+    def test_that_fun_fails_when_not_adding_number(self):
+        self.assertRaises(TypeError, fun, "multiply six by nine")
+
+
+if __name__ == '__main__':
+    unittest.main()
